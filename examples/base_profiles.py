5d06e292451a75d989da2697f6df6f3d10a5838c
cghiaus <72642214+cghiaus@users.noreply.github.com>
2024-11-27 10:13:43 +0100

    Add files via upload

---
+++ examples/base_profiles.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 18:09:13 2024
+
+@author: cghiaus
+
+ELECTRE Tri-B
+with 4 explicitely defined base profiles (for 5 categories).
+"""
+
+import electre_tri as et
+
+# Problem statement
+data_file = "../data/base_profile.csv"
+credibility_threshold = 0.7
+
+# Problem solving
+A, B, T, w = et.read_electre_tri_data(data_file)
+
+opti, pessi = et.electre_tri_b(A, B, T, w,
+                               credibility_threshold)
+
+# Results
+print("\nProbabilistic optimistic ranking:")
+print(opti)
+
+print("\nProbabilistic pessimistic ranking:")
+print(pessi)

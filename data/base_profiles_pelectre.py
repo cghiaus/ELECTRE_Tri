0924f5988dd1a69a67cdd21ab3447ab7e1ca08a4
cghiaus <72642214+cghiaus@users.noreply.github.com>
2024-11-27 09:46:26 +0100

    Add files via upload

---
+++ data/base_profiles_pelectre.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 18:09:13 2024
+
+@author: cghiaus
+
+pELECTRE Tri-B (probabilistic ELECTRE Tri-B)
+with 4 explicitely defined base profiles for 5 categories.
+The standard deviation of the values of performance matrix are in `S`.
+
+"""
+
+import electre_tri as et
+
+folder = '../data/'
+file_std = 'simple_example_std.csv'
+data_file = folder + file_std
+
+A, S, B, T, w = et.read_pelectre_tri_data(data_file)
+credibility_threshold = 0.7
+
+p_opti, p_pessi = et.pelectre_tri_b(
+    A, S, B, T, w,
+    credibility_threshold,
+    n_simulations=100)
+
+print("\nProbabilistic optimistic ranking:")
+print(p_opti)
+
+print("\nProbabilistic pessimistic ranking:")
+print(p_pessi)
